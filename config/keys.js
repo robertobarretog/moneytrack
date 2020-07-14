e78f763df04a4843393f8b7fcc8059c3e482cdeb
Roberto <robert.9524@gmail.com>
2020-07-14 18:59:19 -0400

    Use environment variables for keys

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,8 @@
 import dotenv from 'dotenv';
-import { mongoURI as URI, secretOrKey as secret } from './keys_dev.js';
 
 dotenv.config({ path: './config/config.env' });
 
-let mongoURI, secretOrKey;
-
-if (process.env.NODE_ENV === 'production') {
-  mongoURI = process.env.MONGO_URI;
-  secretOrKey = process.env.SECRET_OR_KEY;
-} else {
-  mongoURI = URI;
-  secretOrKey = secret;
-}
+const mongoURI = process.env.MONGO_URI;
+const secretOrKey = process.env.SECRET_OR_KEY;
 
 export { mongoURI, secretOrKey };
